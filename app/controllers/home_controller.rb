1c9783cfbe001a13540126418724d3e1619d1cb6
Dan Schneiderman <schneiderman.daniel@gmail.com>
2014-02-12 17:04:53 -0500

    Static assets set to true

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-  	@engagementPhotosVert = Dir.glob("app/assets/images/engagement_photos/thumbs/v_*.{jpg}")
-  	@engagementPhotosHoriz = Dir.glob("app/assets/images/engagement_photos/thumbs/h_*.{jpg}")
+    @engagementPhotosHoriz = Dir.glob("app/assets/images/engagement_photos/thumbs/h_*.{jpg}")
   end
 end
