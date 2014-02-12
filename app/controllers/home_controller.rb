2850412a005e699483c4ab41b75a92316b189512
Dan Schneiderman <schneiderman.daniel@gmail.com>
2014-02-12 18:42:53 -0500

    Fallback to assets if images aren't compiled

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @engagementPhotosThumbs = Dir.glob("app/assets/images/engagement_photos/thumbs/h_*.{jpg}")
     @engagementPhotosFull = Dir.glob("app/assets/images/engagement_photos/full/h_*.{jpg}")
   end
 end
